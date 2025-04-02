2dd2cc64ccd513f2a02aa82120e3fce6b3585c8f
Prashant Kaushik <prashantkaushik955@gmail.com>
2025-04-02 13:09:55 +0530

    Change in server.js

--- server.js
+++ server.js
@@ -64,4 +64,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("Development");
 });
